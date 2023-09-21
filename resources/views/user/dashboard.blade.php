4e96de90aaa75b919e7e937862e776dbebf2d96d
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-09-21 14:41:42 +0700

    user dashboard

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,14 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>User</title>
-</head>
-<body>
-    <h1>User only</h1>
+@extends('user.layouts.base')
+@section('content')
+    <div class="px-6 py-10 mx-auto my-3 bg-white shadow lg:my-6 rounded-xl">
+        <h1 class="text-3xl font-bold capitalize lg:text-6xl">{{ Auth::user()->name }}</h1>
+        <p class="text-lg text-gray-500 lg:text-xl">{{ Auth::user()->about }}</p>
+    </div>
+    <div class="px-6 py-10 mx-auto my-3 bg-white shadow lg:my-6 rounded-xl">
+        <h1 class="text-2xl font-bold lg:text-4xl">Goals</h1>
 
-    @dump(Auth::user())
-</body>
-</html>
+        <section>
+            <ul>
+                <li>Hola</li>
+            </ul>
+        </section>
+    </div>
+    <div class="px-6 py-10 mx-auto my-3 bg-white shadow lg:my-6 rounded-xl">
+        <h1 class="text-2xl font-bold lg:text-4xl">Projects</h1>
+
+        <section>
+            <h1>HOla</h1>
+        </section>
+    </div>
+@endsection
---
+++ resources/views/user/layouts/base.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>KarirNote</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    @vite(['resources/css/user/app.css', 'resources/js/user/app.js'])
+</head>
+<body class="antialiased bg-gray-100">
+    <div>
+        @include('user.layouts.nav')
+    </div>
+    <div class="m-3 lg:m-6">
+        @yield('content')
+    </div>
+</body>
+</html>
