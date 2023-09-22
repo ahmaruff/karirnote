bdf6a12532d3be0abb45eb0f6c029c347ee50c83
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-09-22 17:13:20 +0700

    create fillable column in project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'project_objective',
+        'my_contribution',
+        'result',
+    ];
 }
