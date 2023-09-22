b08ca9abf3630efd4b10a980a5111f90ab62c205
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-09-22 15:29:20 +0700

    fix retriving Target models in dashboard

--- app/Http/Controllers/User/Dashboard.php
+++ app/Http/Controllers/User/Dashboard.php
@@ -3,20 +3,28 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Target;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class Dashboard extends Controller
 {
+    private $user;
+
+    public function __construct() {
+        $this->middleware(function ($request, $next) {
+            $this->user = Auth::user();
+            return $next($request);
+        });
+    }
     public function index(): View
     {
-        $user = Auth::user();
-
-        $target = [];
+        $target = Target::orderBy('created_at', 'DESC')->limit(10)->get(['user_id', 'target'])->toArray();
+        // $target = Target::all()->toArray();
         $data = [
-            'name' => $user->name,
-            'about' => $user->about,
+            'name' => $this->user->name,
+            'about' => $this->user->about,
             'target' => $target
         ];
 
