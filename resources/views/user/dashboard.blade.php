7351212855ff05e5930d8f48ab30d2434f707ebc
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-09-22 11:01:49 +0700

    modify dashboard user UI

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,18 +1,19 @@
 @extends('user.layouts.base')
 @section('content')
-
-    <div class="flex flex-col items-stretch justify-between lg:flex-row-reverse">
-        <div class="w-full px-6 py-10 mx-auto my-3 bg-white shadow lg:my-6 rounded-xl">
-            <p>menu</p>
-        </div>
-        <div class="w-full px-6 py-10 mx-auto my-3 bg-white shadow h-max lg:my-6 rounded-xl lg:mr-6">
-            <h1 class="text-3xl font-bold capitalize lg:text-6xl">{{ Auth::user()->name }}</h1>
-            <p class="text-lg text-gray-500 lg:text-xl">{{ Auth::user()->about }}</p>
-        </div>
+    <div class="w-full px-6 py-10 mx-auto my-3 bg-white shadow lg:my-6 rounded-xl">
+        <h1 class="text-3xl font-bold capitalize lg:text-6xl">
+            Lembar Pencapaian Karier
+        </h1>
+        <hr class="my-3">
+        <h2 class="text-xl capitalize lg:text-3xl">
+            {{ Auth::user()->name }}
+        </h2>
+        <p class="text-lg text-gray-500 lg:text-xl">
+            {{ Auth::user()->about }}
+        </p>
     </div>
     <div class="px-6 py-10 mx-auto my-3 bg-white shadow lg:my-6 rounded-xl">
-        <h1 class="text-2xl font-bold lg:text-4xl">Goals</h1>
-
+        <h1 class="text-2xl font-bold lg:text-4xl">Target Pencapaian</h1>
         <section>
             <ul>
                 <li>
@@ -22,8 +23,9 @@
                                 <p>Saya Ingin menjadi CTO Start Up di usia 28 tahun</p>
                             </div>
                             <div class="flex flex-row gap-3">
-                                <x-modal.modal-edit-goals />
-                                <x-modal.modal-hapus-goals />
+                                <x-modal.target-create/>
+                                <x-modal.target-edit/>
+                                <x-modal.target-delete/>
                             </div>
                         </div>
                     </div>
